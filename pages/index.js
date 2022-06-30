a5b2a749b3d2461449c3e54b52886b98430dc07d
gustindvr <gustindvr@gmail.com>
2022-06-30 08:47:06 -0300

    feat: create homepage

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,28 @@ import React from 'react';
 import Head from 'next/head';
 
 import HomePage from '../src/components/organisms/HomePage';
+import { Container, Flex } from '@chakra-ui/react';
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>Portfolio Agustín DVR</title>
         <meta name="description" content="Portfolio de Agustín DVR" />
         <link rel="icon" href="/favicon.ico" />
-        <HomePage />
       </Head>
-    </div>
+      <Container
+        maxW="4xl"
+        my={{ base: '1rem', sm: '1.3rem', md: '2rem', lg: '5rem' }}
+      >
+        <Flex
+          flexFlow="row wrap"
+          justifyContent="space-around"
+          alignItems="center"
+        >
+          <HomePage />
+        </Flex>
+      </Container>
+    </>
   );
 }
--- src/components/organisms/HomePage.jsx
+++ src/components/organisms/HomePage.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
 
+import { Divider, Text, Heading, Box, Image } from '@chakra-ui/react';
+
 const HomePage = () => {
-  return <div>Hola mundo!</div>;
+  return (
+    <>
+      <Box flexBasis="45%">
+        <Heading
+          as="h1"
+          fontSize={{ base: '2.7rem', sm: '3.3em', md: '3.7rem', lg: '4rem' }}
+          textAlign={['center', 'left']}
+          fontFamily="Rampart"
+        >
+          Bienvenidxs a mi portfolio
+        </Heading>
+        <Text
+          as="h2"
+          fontSize={{ base: '1.5rem', sm: '1.3em', md: '1.7rem', lg: '2rem' }}
+          mt="1em"
+        >
+          Soy Agustin Del Valle
+        </Text>
+        <Divider my="0.3em" maxW="xs" />
+        <Text
+          as="h3"
+          fontSize={{ base: '1rem', sm: '1.3em', md: '1.7rem', lg: '2rem' }}
+        >
+          Frontend Developer
+        </Text>
+      </Box>
+      <Box my={{ base: '1rem', sm: 0 }}>
+        <Image
+          src="/casual_3d.png"
+          w={{ base: '200px', sm: '250px', md: '300px' }}
+          h={{ base: '200px', sm: '250px', md: '300px' }}
+        />
+      </Box>
+    </>
+  );
 };
 
 export default HomePage;
